9712ea946e8c5a80fb6f69021b0b826e58af0b13
Kareem Abouelseoud <88090312+kareem8996@users.noreply.github.com>
2025-02-17 23:05:17 +0200

    feat: update system prompts in code generation modules for improved context retrieval

--- Agents/codeGeneration/coder/reflector.py
+++ Agents/codeGeneration/coder/reflector.py
@@ -11,7 +11,7 @@ CONFIGURATIONS={
     'model':"gemini-2.0-flash",
 }
 llm=ChatGoogleGenerativeAI(model=CONFIGURATIONS['model'], temperature=CONFIGURATIONS['temperature'])
-system_prompt = hub.pull("reflector").messages[0].prompt.template
+system_prompt = hub.pull("viz-generation-coder-reflector").messages[0].prompt.template
 
 async def reflector_node(state):
     """
