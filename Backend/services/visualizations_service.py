89391419f3c32b3e1d616affcd317bbcdd6905cd
Kareem Abouelseoud <88090312+kareem8996@users.noreply.github.com>
2025-02-26 20:35:31 +0200

    Change visualization dashboard endpoint to POST and improve error handling in visualization service

--- Backend/services/visualizations_service.py
+++ Backend/services/visualizations_service.py
@@ -77,11 +77,11 @@ class visualizationsService:
     
     async def update_Auto_Gen_Viz(self, project_id: str) -> Tuple[bool, List[str]]:
         try:
-            dataframe=await self.project_service.fetch_dataset(project_id).to_json()
+            dataframe=(await self.project_service.fetch_dataset(project_id)).to_json()
             data_report=await self.project_service.fetch_data_report(project_id)
 
             response=requests.post(f"{self.url}/visualizations/createDashboard",json={'dataframe':dataframe, 'data_report':data_report})
-            serializable_visualizations=json.loads(response.json())
+            serializable_visualizations=json.loads(response.json())['visualizations']
             
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error from Agents Module: {e}")
