87a04c2f3062c2c0d649da46208aa4dd75fa7bb3
MohamedSherif-312 <mshiref31202@gmail.com>
2025-03-03 00:47:26 +0200

    feat: enhance preprocessing pipeline with support for multiple modes and improved message handling

--- Agents/AutoML/Preprocessing/caller.py
+++ Agents/AutoML/Preprocessing/caller.py
@@ -15,37 +15,54 @@ CONFIGURATIONS={
     'temperature':0.7,
     'model':"gemini-2.0-flash",
 }
-class LOGIC(BaseModel):
-    """ A Pydantic model to structure the output of the language model. """
-    logic: str = Field(description="The logic used to preprocess the data.")
-
 
 llm=ChatGoogleGenerativeAI(model=CONFIGURATIONS['model'], temperature=CONFIGURATIONS['temperature'])
 system_prompt = hub.pull("automl-preprocessor-caller").messages[0].prompt.template
 
 async def caller_node(state):
+
     print("Calling Preprocessor Tools")
     project_id = state["project_id"]
     data_report=mainDatabase.fetch_data_report(project_id)
-    if 'preprocessing_messages' not in state or state['preprocessing_messages'] is None:
-        old_messages= []
+    print(f"=======================================\nthis is the state in caller:{state}\n====================================") 
+
+    if state['preprocessing_mode']=='X':
+        if 'X_preprocessing_messages' not in state or state['X_preprocessing_messages'] is None:
+            old_messages= []
+        else:
+            old_messages = state["X_preprocessing_messages"]
     else:
-        old_messages = state["preprocessing_messages"]
+        if 'Y_preprocessing_messages' not in state or state['Y_preprocessing_messages'] is None:
+            old_messages= []
+        else:
+            old_messages = state["Y_preprocessing_messages"]
         
     messages=[
-        {"role": "system", "content":system_prompt+f"\n\n Data Report:\n {data_report}" },
-        {"role": "user", "content": f"Train Feature(s): {state['X_columns']} \n Target Feature: {state['y_column']}\n Preprocessing Logic: {state['preprocessing_logic']}"},
+        {"role": "system", "content":system_prompt },
+        {"role": "user", "content": f"Data Report:\n {data_report} \n\nTrain Feature(s): {state['X_columns']} \n Target Feature: {state['y_column']}\n Preprocessing Logic: {state['X_preprocessing_logic'] if state['preprocessing_mode']=='X' else state['Y_preprocessing_logic']}"},
     ]+old_messages
     model=llm.bind_tools(tools=tools)
     response= await model.ainvoke(messages)
-    return {"preprocessing_messages": [response]}
+
+    if state['preprocessing_mode']=='X':
+        return {"X_preprocessing_messages": [response]}
+    else:
+        return {"Y_preprocessing_messages": [response]}
 
 
 
 
 
-async def should_continue(state)->Literal['tools','__end__']:
-    messages = state["preprocessing_messages"][-1]
+async def should_continue(state)->Literal['tools','__end__','caller_node']:
+    if state['preprocessing_mode']=='X':
+        messages = state["X_preprocessing_messages"][-1]
+    else:
+        messages = state["Y_preprocessing_messages"][-1]
+
     if messages.tool_calls!=[]:
         return "tools"
+    else:
+        if state['preprocessing_mode']=='X':
+            state['preprocessing_mode']='Y'
+            return "caller_node"
     return END
