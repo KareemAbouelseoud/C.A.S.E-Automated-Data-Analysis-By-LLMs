3adc0f1fd193817b74d260f8145bb3ddf1fe97c9
Kareem Abouelseoud <88090312+kareem8996@users.noreply.github.com>
2025-03-22 19:33:25 +0200

    feat(automl): add prediction functionality and update logo asset

--- Frontend/Requests/automlRequests.py
+++ Frontend/Requests/automlRequests.py
@@ -14,3 +14,19 @@ def train(project_id,target_feature,training_features,mode,user_input=None):
                                        'user_input':user_input},stream=True)
     
     return response
+
+def predict(project_id, model_name, data,feature_columns):
+    print("Predicting",data)
+    data_payload = {'model_name': model_name, 'data': data}
+    if feature_columns is not None:
+        data_payload['feature_columns'] = feature_columns
+    
+    response = requests.post(url + f'/project/{project_id}/AutoML/predict/',
+                            json=data_payload)
+    
+    if response.status_code == 200:
+        return (response.json()['predictions'])
+    else:
+        print(f"Failed to predict: HTTP {response.status_code}")
+        print(f"Response: {response.text}",flush=True)
+        return None
