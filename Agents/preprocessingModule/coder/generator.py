17a119db61593c2156e16ebedaae8ffc987af651
MohamedSherif-312 <mshiref31202@gmail.com>
2025-03-29 19:06:31 +0200

    feat(preprocessing): refactor code generation logic to use messages from langsmith hub and improve prompt structure

--- Agents/preprocessingModule/coder/generator.py
+++ Agents/preprocessingModule/coder/generator.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from API.Requests.projectRequests import get_dataset
 from pydantic import BaseModel, Field
+from langchain import hub
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 
@@ -20,47 +21,29 @@ CONFIG = {
 
 #TODO: further improvmements to the system prompt and add to langsmith hub
 llm = ChatGoogleGenerativeAI(model=CONFIG['model'], temperature=CONFIG['temperature'])
-system_prompt = """You are a data preprocessing code generation expert. Create an executable python code that:
-
-1. Processes: preprocessing_step given by the user
-2. Input: DataFrame 'df'
-3. Requirements:
-   - Maintain DataFrame structure
-   - Handle null values appropriately
-   - Preserve original indexes
-   - Add comments explaining key operations
-   - Include safety checks
-
-Constraints:
-- Use pandas/scikit-learn unless impossible
-- No external system calls
-- Validate column existence
-- Include error handling"""
+system_prompt = hub.pull("preprocessing-coder-generator").messages[0].prompt.template
 
 async def generator_node(state):
     """Generate preprocessing code using LLM"""
     print("---GENERATING PREPROCESSING CODE---")
     
-    structured_llm = llm.with_structured_output(PreprocessingCode)
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", system_prompt),
-        ("human", "Preprocessing task: {task_description}\n\nCurrent data schema:\n{data_schema}")
-    ])
-    
-    chain = prompt | structured_llm
+    preprocessing_step = state['preprocessing_steps']
+    target_columns = state['target_columns']
+
+    messages = [
+        {
+            "role": "system",
+            "content": system_prompt
+        },
+        {
+            "role": "user",
+            "content": f"Preprocessing step: {preprocessing_step} \n\n traget column: \n {target_columns}"
+            "please generate the code for the preprocessing step following the required format and the constraints specified by the system"
+        }
+    ]
     
     try:
-        df = await get_dataset(state['project_id'])
-        data_schema = {
-            "columns": df.columns.tolist(),
-            "dtypes": dict(df.dtypes),
-            "sample_data": df.head().to_dict()
-        }
-        
-        code_solution = await chain.ainvoke({
-            "task_description": state['current_step'],
-            "data_schema": data_schema
-        })
+        code_solution = await llm.with_structured_output(PreprocessingCode).ainvoke(messages)
         
         return {
             **state,
